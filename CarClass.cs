175596b1122832a02c9882adc5e4438087a6de34
Theresa Skonieczny <tsscamaro@gmail.com>
2017-04-21 15:15:55 -0400

    first commit

---
+++ CarClass.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class CarClass
+    {
+        private string CarMake;
+        private string CarModel;
+        private int CarYear;
+        private double CarPrice;
+
+        public string CarMake1
+        {
+            get
+            {
+                return CarMake;
+            }
+
+            set
+            {
+                CarMake = value;
+            }
+        }
+
+        public string CarModel1
+        {
+            get
+            {
+                return CarModel;
+            }
+
+            set
+            {
+                CarModel = value;
+            }
+        }
+
+        public int CarYear1
+        {
+            get
+            {
+                return CarYear;
+            }
+
+            set
+            {
+                CarYear = value;
+            }
+        }
+
+        public double CarPrice1
+        {
+            get
+            {
+                return CarPrice;
+            }
+
+            set
+            {
+                CarPrice = value;
+            }
+        }
+
+        //Constructor 
+        public CarClass(string CarMakeInput,string CarModelInput,
+        int CarYearInput, double CarPriceInput)
+    {
+            CarMake1 = CarMakeInput;
+            CarModel1 = CarModelInput;
+            CarYear1 = CarYearInput;
+            CarPrice1 = CarPriceInput;
+    }
+      
+
+        public CarClass()
+        {
+            CarMake1 = "Dodge";
+            CarModel1 = "Journey";
+            CarYear1 = 2017;
+            CarPrice1 = 23000;
+
+        }
+     
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bonus18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //variables
+            string str = new string('*', 20);
+            CarClass CarClass1 = new CarClass();
+            CarClass1.CarMake1 = "";
+            CarClass1.CarModel1 = "";
+            CarClass1.CarYear1 = 0;
+            CarClass1.CarPrice1 = 0;
+
+            //greeting
+            Console.WriteLine("Welcome to Grand Circus Motors Admin Console");
+            Console.WriteLine("");
+
+            //direction to user
+            Console.WriteLine("Enter your cars information, as seen below:");
+            Console.WriteLine("");
+            
+            CarClass Car1 = new CarClass();
+            
+            //input requested
+            Console.WriteLine("Enter the Make of your car: ");
+            Car1.CarMake1 = Console.ReadLine();
+
+            Console.WriteLine("Enter the Model of your car: ");
+            Car1.CarModel1 = Console.ReadLine();
+
+            Console.WriteLine("Enter the Year of your car: ");
+            Car1.CarYear1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the Price of your car: ");
+            Car1.CarPrice1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            //Response from user input 
+            Console.WriteLine("Current Inventory: ");
+            Console.WriteLine("*********************");
+            Console.WriteLine("");
+            Console.Write( Car1.CarMake1);
+            Console.Write("\t");
+            Console.Write(Car1.CarModel1);
+            Console.Write("\t");
+            Console.Write(Car1.CarYear1);
+            Console.Write("\t");
+            Console.Write("$" + Car1.CarPrice1 + "\t" + "\t");
+            Console.WriteLine("");
+
+            //Pretty
+            Console.WriteLine("");
+            Console.WriteLine("*********************");
+            //Salute
+            Console.WriteLine("Thank you! ");
+            Console.WriteLine("");
+
+        }
+    }
+}
